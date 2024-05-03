ddefb4a4d5d61b7f850f8f333e44f6e9ddd8eb29
mdujjal67 <mdujjal.mm67@gmail.com>
2024-05-04 00:02:32 +0600

    added services section

--- src/Pages/Home/Services/ServicesCard.jsx
+++ src/Pages/Home/Services/ServicesCard.jsx
@@ -1,9 +1,10 @@
 import { FaArrowRightLong } from "react-icons/fa6";
+import PropTypes from 'prop-types';
 const ServicesCard = ({ service }) => {
     const { title, img, price } = service
     return (
-        <div className="card card-compact bg-base-100 shadow-lg border p-6 w-full">
-            <figure><img src={img} alt="Shoes" /></figure>
+        <div className="card card-compact bg-base-100 shadow-lg border p-5 aspect-h-3 w-full">
+            <figure><img src={img} alt="Shoes" className="hover:scale-110 transition duration-300 ease-in-out transform" /></figure>
             <div className="card-body">
                 <h2 className="card-title font-bold text-2xl">{title}</h2>
                 <p className="font-bold text-xl text-[#ff3811]">Price: ${price}</p>
@@ -15,5 +16,7 @@ const ServicesCard = ({ service }) => {
         </div>
     );
 };
-
+ServicesCard.propTypes = {
+    service: PropTypes.object
+};
 export default ServicesCard;
--- src/Shared/NavBar/NavBar.jsx
+++ src/Shared/NavBar/NavBar.jsx
@@ -1,28 +1,30 @@
 import { Link } from "react-router-dom";
 import logo from "../../assets/icons/logo.svg"
+import { MdOutlineShoppingBag } from "react-icons/md";
+import { IoSearch } from "react-icons/io5";
+
 
 const NavBar = () => {
     const navItems = <>
-    <li><Link to="/">Home</Link></li>
-    <li><Link to="/about">About</Link></li>
-    <li><Link to="/services">Services</Link></li>
-    <li><Link to="/blog">Blog</Link></li>
-    <li><Link to="/contact">Contact</Link></li>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/services">Services</Link></li>
+        <li><Link to="/blog">Blog</Link></li>
+        <li><Link to="/contact">Contact</Link></li>
     </>
     return (
         <div>
             <div className="navbar bg-base-100 pb-12">
                 <div className="navbar-start">
-                    <div className="dropdown">
-                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-                            <img src={logo} alt="" />
+                    <div className="dropdown block lg:hidden">
+                        <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
                         </div>
                         <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
                             {navItems}
                         </ul>
                     </div>
-                   <Link to="/"><img src={logo} alt="logo-image" className="w-24 "/></Link>
-
+                <Link to="/"><img src={logo} alt="logo-image" className="w-[70px] lg:w-24 ml-4 lg:ml-0" /></Link>
                 </div>
                 <div className="navbar-center hidden lg:flex">
                     <ul className="menu menu-horizontal px-1">
@@ -30,6 +32,8 @@ const NavBar = () => {
                     </ul>
                 </div>
                 <div className="navbar-end">
+                    <button><MdOutlineShoppingBag className="text-xl hover:text-orange-500"/></button>
+                    <button><IoSearch  className="text-xl mx-7 hover:text-orange-500"/></button>
                     <button className="btn btn-outline btn-warning">Appointment</button>
                 </div>
             </div>
